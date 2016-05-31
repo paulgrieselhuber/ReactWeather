7eeee35a03a80b776cd6efa4566b837f67f3d21e
Paul Grieselhuber <paul@loudnoises.us>
2016-05-31 16:40:50 +0200

    About page update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,12 @@
 var React = require('react')
 
-var About = (props) => <h3>About Component</h3>
+var About = (props) => {
+    return (
+      <div>
+        <h3>About</h3>
+        <p>Welcome to the about page.</p>
+      </div>
+    )
+}
 
 module.exports = About
